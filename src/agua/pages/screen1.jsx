4a5fb27b2f8faa426049e2708bfe6682d9a72914
david-dom <113909984+david-dom@users.noreply.github.com>
2023-08-25 09:28:20 -0500

    My commit message

---
+++ src/agua/pages/screen1.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './screen1.module.css';
+
+
+
+function Screen1() {
+    
+
+    return(
+        <div className={styles["Screen1"]}>
+            
+<div id="layer-1-9f32a6d2c191417ab8b6e5607be8d5b5" className={styles["layer-1-9f32a6d2c191417ab8b6e5607be8d5b5"]} ></div>
+<div id="layer-2-d2d569512628432d9e90b13b7b03ba0a" className={styles["layer-2-d2d569512628432d9e90b13b7b03ba0a"]} >
+<div id="layer-3-c2d44d3784ea4d518db7d3561087a72c" className={styles["layer-3-c2d44d3784ea4d518db7d3561087a72c"]} ></div>
+<div id="layer-4-33abcced0c09402c96c2cc7ff09f4fbb" className={styles["layer-4-33abcced0c09402c96c2cc7ff09f4fbb"]} >
+<div id="layer-5-52018ac4fc4146c89afd2f8bd9532344" className={styles["layer-5-52018ac4fc4146c89afd2f8bd9532344"]} >
+<div id="text-9-d51f4cdb819043db8eec406f93cffb20" className={styles["text-9-d51f4cdb819043db8eec406f93cffb20"]} >1. Qualified Purchasers receive an Apple Gift Card when they purchase an eligible Mac or iPad at a Qualifying Location. Only one Apple Gift Card per eligible Mac or iPad per Qualified Purchaser. Offer subject to availability. While supplies last. View full terms and conditions of offer here.</div>
+<div id="text-8-2f2f3579fb9648189acb7d48b376a81e" className={styles["text-8-2f2f3579fb9648189acb7d48b376a81e"]} >2. $4.99/month after free trial. One subscription per Family Sharing group. Offer good for 3 months after eligible device activation. Plan automatically renews until cancelled. Restrictions and other terms apply.</div>
+<div id="text-3-1dee08b81bed44e3a27f2364b1a0b193" className={styles["text-3-1dee08b81bed44e3a27f2364b1a0b193"]} >To access and use all the features of Apple Card, you must add Apple Card to Wallet on an iPhone or iPad with the latest version of iOS or iPadOS. Update to the latest version by going to Settings > General > Software Update. Tap Download and Install.</div>
+<div id="text-4-79f4b9bb45eb4a97aa3cdce497462ad5" className={styles["text-4-79f4b9bb45eb4a97aa3cdce497462ad5"]} >If you reside in the U.S. territories, please call Goldman Sachs at 877-255-5923 with questions about Apple Card.</div>
+<div id="text-12-433181d9cfbf4d488ff851f00f685645" className={styles["text-12-433181d9cfbf4d488ff851f00f685645"]} >Learn more about how Apple Card applications are evaluated at support.apple.com/kb/HT209218.
+</div>
+<div id="text-7-c30327a6bea44a5b9dad3d27a4062e3a" className={styles["text-7-c30327a6bea44a5b9dad3d27a4062e3a"]} >A subscription is required for Apple TV+.</div>
+<div id="text-11-f22621a38a2b46fa9b392f487937c79e" className={styles["text-11-f22621a38a2b46fa9b392f487937c79e"]} >Major League Baseball trademarks and copyrights are used with permission of MLB Advanced Media, L.P. All rights reserved.
+</div>
+</div>
+<div id="layer-7-3103820f709f4235a2a2d89c70bca0e0" className={styles["layer-7-3103820f709f4235a2a2d89c70bca0e0"]} ></div>
+<div id="layer-6-9217fb47f286408da496ec2a025240af" className={styles["layer-6-9217fb47f286408da496ec2a025240af"]} >
+<div id="text-10-f65edd550b114ecfab5c80fda2a810d5" className={styles["text-10-f65edd550b114ecfab5c80fda2a810d5"]} >[Text]</div>
+<div id="text-13-2e593d8849124e60976e54d43c858093" className={styles["text-13-2e593d8849124e60976e54d43c858093"]} >[Text]</div>
+<div id="text-14-c0966b2c63694542bf1afb658f135aa3" className={styles["text-14-c0966b2c63694542bf1afb658f135aa3"]} >[Text]</div>
+<div id="text-15-bed93bb4fb504c888c702165f0c482db" className={styles["text-15-bed93bb4fb504c888c702165f0c482db"]} >[Text]</div>
+<div id="text-16-faca6a0bc12d4c04953397cd062fd5ed" className={styles["text-16-faca6a0bc12d4c04953397cd062fd5ed"]} >[Text]</div>
+</div>
+<div id="layer-8-333f8a810b4f44929b333e80a278338d" className={styles["layer-8-333f8a810b4f44929b333e80a278338d"]} >
+<div id="text-17-cef86ad423ca45ec8b7308ea4129d0dd" className={styles["text-17-cef86ad423ca45ec8b7308ea4129d0dd"]} >[Text]</div>
+<div id="layer-9-13a80f6cd11a4ff4963eb9d2e1051e30" className={styles["layer-9-13a80f6cd11a4ff4963eb9d2e1051e30"]} >
+<div id="text-18-e16f71580a4b4a7c8ca2cfcfe0b14545" className={styles["text-18-e16f71580a4b4a7c8ca2cfcfe0b14545"]} >[Text]</div>
+<div id="text-19-5e33916e84f243e8b154641245f6d46f" className={styles["text-19-5e33916e84f243e8b154641245f6d46f"]} >[Text]</div>
+<div id="text-20-8f225d4b36c947ce83f7316081df5bd4" className={styles["text-20-8f225d4b36c947ce83f7316081df5bd4"]} >[Text]</div>
+<div id="text-21-1671dd0a8f194b4eae16b4046deea687" className={styles["text-21-1671dd0a8f194b4eae16b4046deea687"]} >[Text]</div>
+<div id="text-22-07b6d983ea404b6fb13796e4030815a5" className={styles["text-22-07b6d983ea404b6fb13796e4030815a5"]} >[Text]</div>
+</div>
+<div id="text-23-deb545b8785e4aea8e943250e927d876" className={styles["text-23-deb545b8785e4aea8e943250e927d876"]} >[Text]</div>
+</div>
+</div>
+</div>
+        </div>
+    );
+}
+
+export default Screen1;
+            
