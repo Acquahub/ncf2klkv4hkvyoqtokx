2472f6946860679a02e2891226c2bc73e984dbed
david-dom <113909984+david-dom@users.noreply.github.com>
2023-08-25 09:43:44 -0500

    My commit message

--- src/agua/pages/screen1.jsx
+++ src/agua/pages/screen1.jsx
@@ -25,22 +25,124 @@ function Screen1() {
 <div id="text-11-f22621a38a2b46fa9b392f487937c79e" className={styles["text-11-f22621a38a2b46fa9b392f487937c79e"]} >Major League Baseball trademarks and copyrights are used with permission of MLB Advanced Media, L.P. All rights reserved.
 </div>
 </div>
-<div id="layer-7-3103820f709f4235a2a2d89c70bca0e0" className={styles["layer-7-3103820f709f4235a2a2d89c70bca0e0"]} ></div>
+<div id="layer-7-3103820f709f4235a2a2d89c70bca0e0" className={styles["layer-7-3103820f709f4235a2a2d89c70bca0e0"]} >
+<div id="layer-10-50d2b71ca0854687a8b551e58cd4cd29" className={styles["layer-10-50d2b71ca0854687a8b551e58cd4cd29"]} >
+<div id="layer-15-3a15308f7af84162a350344cd71ef192" className={styles["layer-15-3a15308f7af84162a350344cd71ef192"]} >
+<div id="text-20-02d14d4d273843c193d9d419245573e9" className={styles["text-20-02d14d4d273843c193d9d419245573e9"]} >[Text]</div>
+<div id="text-21-31ce004b11ee4141a534097b51398beb" className={styles["text-21-31ce004b11ee4141a534097b51398beb"]} >[Text]</div>
+<div id="text-22-b399a9d05a4c423d95747fda9645e497" className={styles["text-22-b399a9d05a4c423d95747fda9645e497"]} >[Text]</div>
+<div id="text-23-92fa7832648a46dc8dc57c96e18a17c4" className={styles["text-23-92fa7832648a46dc8dc57c96e18a17c4"]} >[Text]</div>
+<div id="text-24-a8ed712bccd848acb0ec4676db8233eb" className={styles["text-24-a8ed712bccd848acb0ec4676db8233eb"]} >[Text]</div>
+<div id="text-25-583dc18ad2824c5e88c8f91f11f04575" className={styles["text-25-583dc18ad2824c5e88c8f91f11f04575"]} >[Text]</div>
+<div id="text-26-08582ae3881f41bd99352fc2ff4d3fd8" className={styles["text-26-08582ae3881f41bd99352fc2ff4d3fd8"]} >[Text]</div>
+<div id="text-27-d4128479e8074b51b277a021cd5146dc" className={styles["text-27-d4128479e8074b51b277a021cd5146dc"]} >[Text]</div>
+<div id="text-28-d2eae7f364f54cdfbfa09dbc881b1698" className={styles["text-28-d2eae7f364f54cdfbfa09dbc881b1698"]} >[Text]</div>
+<div id="text-29-0da4e453b1164fdfad1543265f241467" className={styles["text-29-0da4e453b1164fdfad1543265f241467"]} >[Text]</div>
+<div id="text-30-63802736b17144a8909485fe92a35987" className={styles["text-30-63802736b17144a8909485fe92a35987"]} >[Text]</div>
+</div>
+<div id="layer-16-31d6e8f09a134d72b63890e1a78de148" className={styles["layer-16-31d6e8f09a134d72b63890e1a78de148"]} >
+<div id="text-31-b80845d21cf64a8cac1c48ac28340456" className={styles["text-31-b80845d21cf64a8cac1c48ac28340456"]} >[Text]</div>
+<div id="text-32-cde58f064fc64ab69eb6262e9f1b9458" className={styles["text-32-cde58f064fc64ab69eb6262e9f1b9458"]} >[Text]</div>
+<div id="text-33-9769143f9eaa433298c6e1ab59de224e" className={styles["text-33-9769143f9eaa433298c6e1ab59de224e"]} >[Text]</div>
+<div id="text-34-2ac25dce07f445b783437556616d2049" className={styles["text-34-2ac25dce07f445b783437556616d2049"]} >[Text]</div>
+<div id="text-35-a3b92b54e5b142e1bf37ed4f8c8e1e81" className={styles["text-35-a3b92b54e5b142e1bf37ed4f8c8e1e81"]} >[Text]</div>
+</div>
+</div>
+<div id="layer-11-b47f069fba6c44aeb68f7da90755e9ac" className={styles["layer-11-b47f069fba6c44aeb68f7da90755e9ac"]} >
+<div id="layer-17-5b2e261b1f564404800a82e20651dc51" className={styles["layer-17-5b2e261b1f564404800a82e20651dc51"]} >
+<div id="text-36-e5e396cbdafb486f81027a6b663c2d44" className={styles["text-36-e5e396cbdafb486f81027a6b663c2d44"]} >[Text]</div>
+<div id="text-37-20b106b3f2ca41068dfb0df7890141df" className={styles["text-37-20b106b3f2ca41068dfb0df7890141df"]} >[Text]</div>
+<div id="text-38-be1b499e53224a88abf72a89ef7368be" className={styles["text-38-be1b499e53224a88abf72a89ef7368be"]} >[Text]</div>
+<div id="text-39-914c3d6c21cb4b4d9320bc3530ef1032" className={styles["text-39-914c3d6c21cb4b4d9320bc3530ef1032"]} >[Text]</div>
+</div>
+<div id="layer-18-d7814763af554800a2282432c57231b8" className={styles["layer-18-d7814763af554800a2282432c57231b8"]} >
+<div id="text-40-53afdb5f683747c59ea3751a58afeece" className={styles["text-40-53afdb5f683747c59ea3751a58afeece"]} >[Text]</div>
+<div id="text-41-a7a6209519e948c8a6e7d7986627b55a" className={styles["text-41-a7a6209519e948c8a6e7d7986627b55a"]} >[Text]</div>
+<div id="text-42-7efe87535f6241b69828e0210117b32e" className={styles["text-42-7efe87535f6241b69828e0210117b32e"]} >[Text]</div>
+<div id="text-43-5f22b5c6bb4b41b3b1175d2a9d841683" className={styles["text-43-5f22b5c6bb4b41b3b1175d2a9d841683"]} >[Text]</div>
+<div id="text-44-be8c6c67514c4a13bcf4b2706ed76027" className={styles["text-44-be8c6c67514c4a13bcf4b2706ed76027"]} >[Text]</div>
+<div id="text-45-eb49b37a11ca49c48af034ccd3d0e504" className={styles["text-45-eb49b37a11ca49c48af034ccd3d0e504"]} >[Text]</div>
+<div id="text-46-5d28ac5989f643c08f31b1cfff8efc1f" className={styles["text-46-5d28ac5989f643c08f31b1cfff8efc1f"]} >[Text]</div>
+<div id="text-47-c8df0aec95ae4d3f8633d53aa53e8162" className={styles["text-47-c8df0aec95ae4d3f8633d53aa53e8162"]} >[Text]</div>
+<div id="text-48-ee46f7064a944071a5aa73fde2285e0b" className={styles["text-48-ee46f7064a944071a5aa73fde2285e0b"]} >[Text]</div>
+<div id="text-49-53cc0ca889da4715a331a016f765b6a6" className={styles["text-49-53cc0ca889da4715a331a016f765b6a6"]} >[Text]</div>
+</div>
+</div>
+<div id="layer-12-0fabace657f546d68cff082ab73d909e" className={styles["layer-12-0fabace657f546d68cff082ab73d909e"]} >
+<div id="text-50-561b832c6f8348be8afaa7a29bf72948" className={styles["text-50-561b832c6f8348be8afaa7a29bf72948"]} >[Text]</div>
+<div id="text-51-97e5140c8b5b499f8500aea7d8af8bec" className={styles["text-51-97e5140c8b5b499f8500aea7d8af8bec"]} >[Text]</div>
+<div id="text-52-d6638111aceb453cbac8070890e19ebd" className={styles["text-52-d6638111aceb453cbac8070890e19ebd"]} >[Text]</div>
+<div id="text-53-0d396f5b0f18457991de71fb3f431001" className={styles["text-53-0d396f5b0f18457991de71fb3f431001"]} >[Text]</div>
+<div id="text-54-774fe03e67b4409c839dd4499cd1ffd6" className={styles["text-54-774fe03e67b4409c839dd4499cd1ffd6"]} >[Text]</div>
+<div id="text-55-b1d435e156954cefbabb9b596a5bc9b2" className={styles["text-55-b1d435e156954cefbabb9b596a5bc9b2"]} >[Text]</div>
+<div id="text-56-0eb8b5f693f649a0abfaf55c3cb8703e" className={styles["text-56-0eb8b5f693f649a0abfaf55c3cb8703e"]} >[Text]</div>
+<div id="text-57-0e3cbeea7c3043919a029e757c705a82" className={styles["text-57-0e3cbeea7c3043919a029e757c705a82"]} >[Text]</div>
+<div id="text-58-d308f445a57c4f459a428cd6b3fcaf65" className={styles["text-58-d308f445a57c4f459a428cd6b3fcaf65"]} >[Text]</div>
+<div id="text-59-35e88353f6984820992d66756d0eb8ba" className={styles["text-59-35e88353f6984820992d66756d0eb8ba"]} >[Text]</div>
+<div id="text-60-67cf866678514fdfa000b36821132476" className={styles["text-60-67cf866678514fdfa000b36821132476"]} >[Text]</div>
+</div>
+<div id="layer-13-e7bf93adad1d496fa03ffc8b31c70ca3" className={styles["layer-13-e7bf93adad1d496fa03ffc8b31c70ca3"]} >
+<div id="layer-19-614e46323e4046ec8e79b1ad000c5d93" className={styles["layer-19-614e46323e4046ec8e79b1ad000c5d93"]} >
+<div id="text-61-3e68387478ed4b76872436873b86ddc8" className={styles["text-61-3e68387478ed4b76872436873b86ddc8"]} >[Text]</div>
+<div id="text-62-ee605f2f2bee4066a76a7d8aa7309fd5" className={styles["text-62-ee605f2f2bee4066a76a7d8aa7309fd5"]} >[Text]</div>
+<div id="text-63-f68bff503d254a17b74fbb3d1134c4b8" className={styles["text-63-f68bff503d254a17b74fbb3d1134c4b8"]} >[Text]</div>
+</div>
+<div id="layer-20-f4361ad911884dea90ef618b9488edcd" className={styles["layer-20-f4361ad911884dea90ef618b9488edcd"]} >
+<div id="text-64-177cd413ca9b4e4ca256f676252587f5" className={styles["text-64-177cd413ca9b4e4ca256f676252587f5"]} >[Text]</div>
+<div id="text-65-22f5a3f76bbf44008457d34c548e2ebf" className={styles["text-65-22f5a3f76bbf44008457d34c548e2ebf"]} >[Text]</div>
+<div id="text-66-f15a69a7f63443f9b34fcef551d5bb5c" className={styles["text-66-f15a69a7f63443f9b34fcef551d5bb5c"]} >[Text]</div>
+<div id="text-67-a29d0e3bcc0f4dc497027d75f93da377" className={styles["text-67-a29d0e3bcc0f4dc497027d75f93da377"]} >[Text]</div>
+</div>
+<div id="layer-22-f8e79955a85b481599e2a9a3a69b746e" className={styles["layer-22-f8e79955a85b481599e2a9a3a69b746e"]} >
+<div id="text-68-168b400818dc4f83b5e75bb7883823ef" className={styles["text-68-168b400818dc4f83b5e75bb7883823ef"]} >[Text]</div>
+<div id="text-69-6144f68ccaf54617af93a3e62d610b4f" className={styles["text-69-6144f68ccaf54617af93a3e62d610b4f"]} >[Text]</div>
+<div id="text-70-d957882401b14ee4b64b1b0db83db3c3" className={styles["text-70-d957882401b14ee4b64b1b0db83db3c3"]} >[Text]</div>
+<div id="text-71-f63a04c81c4b4b3f96d00fd85e880ba2" className={styles["text-71-f63a04c81c4b4b3f96d00fd85e880ba2"]} >[Text]</div>
+</div>
+<div id="layer-21-a68b7c80eeb5423da2d3a0e3b99ef8d0" className={styles["layer-21-a68b7c80eeb5423da2d3a0e3b99ef8d0"]} >
+<div id="text-72-5b7b97197d3340a6a5d2c997a216b959" className={styles["text-72-5b7b97197d3340a6a5d2c997a216b959"]} >[Text]</div>
+<div id="text-73-d6d16b36cc444feaa12284725972634f" className={styles["text-73-d6d16b36cc444feaa12284725972634f"]} >[Text]</div>
+<div id="text-74-00ca521683054304bd325988b212d8b4" className={styles["text-74-00ca521683054304bd325988b212d8b4"]} >[Text]</div>
+</div>
+</div>
+<div id="layer-14-a4458370fdaa4cccb3a09054a71e9efb" className={styles["layer-14-a4458370fdaa4cccb3a09054a71e9efb"]} >
+<div id="layer-23-deb23efca6404332a7291f7a051bdaa3" className={styles["layer-23-deb23efca6404332a7291f7a051bdaa3"]} >
+<div id="text-75-1ddd2652de304a72a568c4998aa9b1ea" className={styles["text-75-1ddd2652de304a72a568c4998aa9b1ea"]} >[Text]</div>
+<div id="text-76-795e55bf47754ddd902ee1d8ac7ea08f" className={styles["text-76-795e55bf47754ddd902ee1d8ac7ea08f"]} >[Text]</div>
+<div id="text-77-cd6e10fd3eb04da0ab3d92a61b6971c1" className={styles["text-77-cd6e10fd3eb04da0ab3d92a61b6971c1"]} >[Text]</div>
+<div id="text-78-508fb507d15045ff8cd1f41fea96b36d" className={styles["text-78-508fb507d15045ff8cd1f41fea96b36d"]} >[Text]</div>
+<div id="text-79-08ed265f7eb144639a16402e872a72d4" className={styles["text-79-08ed265f7eb144639a16402e872a72d4"]} >[Text]</div>
+<div id="text-80-4cf15eb71ae84473b590f0bd650512cb" className={styles["text-80-4cf15eb71ae84473b590f0bd650512cb"]} >[Text]</div>
+<div id="text-81-c2fa63a10f024b1a8d8ebe0d44f25c7a" className={styles["text-81-c2fa63a10f024b1a8d8ebe0d44f25c7a"]} >[Text]</div>
+<div id="text-82-18a2f6ef4e914c50a1cf217262c2eccb" className={styles["text-82-18a2f6ef4e914c50a1cf217262c2eccb"]} >[Text]</div>
+</div>
+<div id="layer-24-0e311d256cee437a8839836e1b9cd333" className={styles["layer-24-0e311d256cee437a8839836e1b9cd333"]} >
+<div id="text-83-c94e0949a03041b7aa288f7b5bf25b17" className={styles["text-83-c94e0949a03041b7aa288f7b5bf25b17"]} >[Text]</div>
+<div id="text-84-4f982a18993a4753a4e7725d24e845a4" className={styles["text-84-4f982a18993a4753a4e7725d24e845a4"]} >[Text]</div>
+<div id="text-85-e30fc693f0694208b6085c0b8384e1cf" className={styles["text-85-e30fc693f0694208b6085c0b8384e1cf"]} >[Text]</div>
+<div id="text-86-ab123d29e28643129a1ff6fafc8ed771" className={styles["text-86-ab123d29e28643129a1ff6fafc8ed771"]} >[Text]</div>
+<div id="text-87-ef982ee6c542495eb81bf41518072c47" className={styles["text-87-ef982ee6c542495eb81bf41518072c47"]} >[Text]</div>
+<div id="text-88-7118cfeaceff46d1ad0c6f40af262818" className={styles["text-88-7118cfeaceff46d1ad0c6f40af262818"]} >[Text]</div>
+<div id="text-89-031be7df1223440f8034af1f225fc1b6" className={styles["text-89-031be7df1223440f8034af1f225fc1b6"]} >[Text]</div>
+<div id="text-90-c7995a5dc8c048c68ade39b3b82a64c8" className={styles["text-90-c7995a5dc8c048c68ade39b3b82a64c8"]} >[Text]</div>
+</div>
+</div>
+</div>
 <div id="layer-6-9217fb47f286408da496ec2a025240af" className={styles["layer-6-9217fb47f286408da496ec2a025240af"]} >
-<div id="text-10-f65edd550b114ecfab5c80fda2a810d5" className={styles["text-10-f65edd550b114ecfab5c80fda2a810d5"]} >[Text]</div>
-<div id="text-13-2e593d8849124e60976e54d43c858093" className={styles["text-13-2e593d8849124e60976e54d43c858093"]} >[Text]</div>
-<div id="text-14-c0966b2c63694542bf1afb658f135aa3" className={styles["text-14-c0966b2c63694542bf1afb658f135aa3"]} >[Text]</div>
-<div id="text-15-bed93bb4fb504c888c702165f0c482db" className={styles["text-15-bed93bb4fb504c888c702165f0c482db"]} >[Text]</div>
-<div id="text-16-faca6a0bc12d4c04953397cd062fd5ed" className={styles["text-16-faca6a0bc12d4c04953397cd062fd5ed"]} >[Text]</div>
+<div id="text-10-f65edd550b114ecfab5c80fda2a810d5" className={styles["text-10-f65edd550b114ecfab5c80fda2a810d5"]} >More ways to shop:</div>
+<div id="text-13-2e593d8849124e60976e54d43c858093" className={styles["text-13-2e593d8849124e60976e54d43c858093"]} >Find an Apple Store </div>
+<div id="text-14-c0966b2c63694542bf1afb658f135aa3" className={styles["text-14-c0966b2c63694542bf1afb658f135aa3"]} >or</div>
+<div id="text-15-bed93bb4fb504c888c702165f0c482db" className={styles["text-15-bed93bb4fb504c888c702165f0c482db"]} >other retailer</div>
+<div id="text-16-faca6a0bc12d4c04953397cd062fd5ed" className={styles["text-16-faca6a0bc12d4c04953397cd062fd5ed"]} >near you. Or call 1-800-MY-APPLE.</div>
 </div>
 <div id="layer-8-333f8a810b4f44929b333e80a278338d" className={styles["layer-8-333f8a810b4f44929b333e80a278338d"]} >
-<div id="text-17-cef86ad423ca45ec8b7308ea4129d0dd" className={styles["text-17-cef86ad423ca45ec8b7308ea4129d0dd"]} >[Text]</div>
+<div id="text-17-cef86ad423ca45ec8b7308ea4129d0dd" className={styles["text-17-cef86ad423ca45ec8b7308ea4129d0dd"]} >Copyright © 2023 Apple Inc. All rights reserved.</div>
 <div id="layer-9-13a80f6cd11a4ff4963eb9d2e1051e30" className={styles["layer-9-13a80f6cd11a4ff4963eb9d2e1051e30"]} >
-<div id="text-18-e16f71580a4b4a7c8ca2cfcfe0b14545" className={styles["text-18-e16f71580a4b4a7c8ca2cfcfe0b14545"]} >[Text]</div>
-<div id="text-19-5e33916e84f243e8b154641245f6d46f" className={styles["text-19-5e33916e84f243e8b154641245f6d46f"]} >[Text]</div>
-<div id="text-20-8f225d4b36c947ce83f7316081df5bd4" className={styles["text-20-8f225d4b36c947ce83f7316081df5bd4"]} >[Text]</div>
-<div id="text-21-1671dd0a8f194b4eae16b4046deea687" className={styles["text-21-1671dd0a8f194b4eae16b4046deea687"]} >[Text]</div>
-<div id="text-22-07b6d983ea404b6fb13796e4030815a5" className={styles["text-22-07b6d983ea404b6fb13796e4030815a5"]} >[Text]</div>
+<div id="text-18-e16f71580a4b4a7c8ca2cfcfe0b14545" className={styles["text-18-e16f71580a4b4a7c8ca2cfcfe0b14545"]} >Privacy Policy</div>
+<div id="text-19-5e33916e84f243e8b154641245f6d46f" className={styles["text-19-5e33916e84f243e8b154641245f6d46f"]} >Terms of Use</div>
+<div id="text-20-8f225d4b36c947ce83f7316081df5bd4" className={styles["text-20-8f225d4b36c947ce83f7316081df5bd4"]} >Sales and Refunds</div>
+<div id="text-21-1671dd0a8f194b4eae16b4046deea687" className={styles["text-21-1671dd0a8f194b4eae16b4046deea687"]} >Legal</div>
+<div id="text-22-07b6d983ea404b6fb13796e4030815a5" className={styles["text-22-07b6d983ea404b6fb13796e4030815a5"]} >Site Map</div>
 </div>
 <div id="text-23-deb545b8785e4aea8e943250e927d876" className={styles["text-23-deb545b8785e4aea8e943250e927d876"]} >[Text]</div>
 </div>
